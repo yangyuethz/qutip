4a4aceda659e33eb3babc8f20d11741ebc3e44ef
yangyuethz <49004531+yangyuethz@users.noreply.github.com>
2021-06-03 14:04:04 +0200

    Update cd20210310.py

--- cd20210310.py
+++ cd20210310.py
@@ -21,9 +21,9 @@ reload(hbar_simulation_class)
 #qubit dimission, we only consider g and e states here
 qubit_dim=2
 #phonon dimission
-phonon_dim=5
+phonon_dim=3
 #how many phonon modes we consider here
-phonon_num=1
+phonon_num=2
 #the frequency difference between qubit and phonon (qubit minus phonon)
 qubit_phonon_detuning=5968.6-5974.1
 
@@ -34,8 +34,8 @@ t1=[11]+[77]*(phonon_num)
 #T2 list of the system
 t2=[12]+[104]*(phonon_num)
 #set up the processor and compiler,qb5d97 is the qubit we play around
-qb5d97_processor=hbar_processor.HBAR_processor((phonon_num+1),t1,t2,dims,g=[0.26],\
-    rest_place=qubit_phonon_detuning,FSR=12)
+qb5d97_processor=hbar_processor.HBAR_processor((phonon_num+1),t1,t2,dims,g=[0.26,0.07],\
+    rest_place=qubit_phonon_detuning,FSR=1.1)
 qb5d97_compiler = hbar_compiler.HBAR_Compiler(qb5d97_processor.num_qubits,\
     qb5d97_processor.params, qb5d97_processor.pulse_dict)
 qb5d97_simulation=hbar_simulation_class.Simulation(qb5d97_processor,qb5d97_compiler)
@@ -55,13 +55,22 @@ qb5d97_simulation.swap_time_list=[0.9615913396391608,
 '''
 For higher order phonon rabi
 '''
-for i in range(0,10):
-    qb5d97_simulation.generate_fock_state(i)
-    qb5d97_simulation.phonon_rabi_measurement()                              
-#%%
+y_list=[]
+qb5d97_simulation.t_list=np.linspace(0.01,10,100)
+detuning_list=np.linspace(-0.1,0.1,21)
+for detuning in detuning_list:
+    qb5d97_simulation.generate_fock_state(0)
+    qb5d97_simulation.phonon_rabi_measurement(detuning=detuning)  
+    y_list.append(qb5d97_simulation.y_array)
 
+#%%
+xx,yy=np.meshgrid(detuning_list,qb5d97_simulation.t_list)
+plt.pcolormesh(xx,yy,np.array(y_list).transpose())
+plt.ylabel('(us)')
+plt.xlabel('detuning(MHz)')
+#%%
 '''
-This part is for calibrating probe amplitude for certain pulse length
+This part is for calibrating probe amplitude or pulse length,
 '''
 param_probe={'Omega':0.015,
     'sigma': 0.5,
@@ -69,19 +78,21 @@ param_probe={'Omega':0.015,
     'amplitude_starkshift':0}
 
 y_list=[]
-probe_omega_list=np.linspace(0.015,0.0225,2)
-for probe_omega in probe_omega_list:
-    param_probe['Omega']=probe_omega
+sweep_list=np.linspace(0.005,0.02,10)
+for sweep_data in sweep_list:
+    param_probe['Omega']=sweep_data
+    param_probe['duration']=30
     qb5d97_simulation.ideal_phonon_fock(0)
     param_probe['amplitude_starkshift']=5973.3-5968.6
     qb5d97_simulation.detuning_list=np.linspace(
-        param_probe['amplitude_starkshift']-0.4,
-        param_probe['amplitude_starkshift']+0.4,81)
+        param_probe['amplitude_starkshift']-0.2,
+        param_probe['amplitude_starkshift']+0.2,41)
     qb5d97_simulation.spec_measurement(param_probe)
     y_list.append(qb5d97_simulation.y_array)
+
 figure,ax = plt.subplots(figsize=(8,6))
-for i,probe_omega in enumerate(probe_omega_list):
-    ax.plot(qb5d97_simulation.detuning_list ,y_list[i],label='probe omega={}'.format(probe_omega))
+for i,sweep_data in enumerate(sweep_list):
+    ax.plot(qb5d97_simulation.detuning_list ,y_list[i],label='probe omega={}MHz'.format(sweep_data))
 figure.legend()
 figure.show()
 
@@ -97,9 +108,9 @@ param_drive={'Omega':0.1,
     'rotate_direction':np.pi/4
     }
 
-param_probe={'Omega':0.015,
+param_probe={'Omega':0.01,
     'sigma': 0.5,
-    'duration':15.75,
+    'duration':30,
     'amplitude_starkshift':0}
 
 #define the parameter for probing the qubit
@@ -110,9 +121,9 @@ for fock_number in fock_number_list:
     # qb5d97_simulation.ideal_phonon_fock(fock_number)
     # param_drive['detuning']=-qubit_phonon_detuning
     # qb5d97_simulation.generate_coherent_state(phonon_drive_params=param_drive)
-    param_probe['amplitude_starkshift']=5973.3-5968.6
+    param_probe['amplitude_starkshift']=5972.3-5968.6
     qb5d97_simulation.detuning_list=np.linspace(
-        param_probe['amplitude_starkshift']-0.8,
+        param_probe['amplitude_starkshift']-0.6,
         param_probe['amplitude_starkshift']+0.2,100)
     qb5d97_simulation.spec_measurement(param_probe)
     y_list.append(qb5d97_simulation.y_array)
